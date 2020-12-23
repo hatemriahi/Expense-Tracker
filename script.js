7f07c5d4505d5b0af5b7f9a2c129d915cd63449e
Hatem Riahi <riahihaatem@gmail.com>
2020-12-23 19:55:30 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+
+const dummyTransactions = [
+    {id:1 ,text:'Cash',amount:-20},
+    {id:2 ,text:'Paycheck',amount:100},
+];
+
+
